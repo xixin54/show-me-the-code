e31b2f912a178204e27121ef3a7fa1d44320ebd2
xixin54 <xixin54@users.noreply.github.com>
2017-04-19 19:13:10 +0800

    Update MissSloane.py

--- MissSloane.py
+++ MissSloane.py
@@ -4,8 +4,13 @@
 # -*- coding:utf-8 -*-
 
 import requests
-import beautifulsoup fro bs4
+import BeautifulSoup fro bs4
+import re
 #import lxml
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 
 cookie =''
 
@@ -19,8 +24,20 @@ for url in generator_URL():
   print url      # test generator
     
 def Slonae(url):
+  dict1 = {}
   for i in url:
-    res = resquest(i,cookie,time=100)
+    res = resquest(i,cookie,timeout=100)
+    soup = BeautifulSoup(res)
+    comture = soup.findAll(class='comment')
+    
+    #dict1 = {}
+    for i in comture:
+      name = i.find(class='comment-info').a.string
+      comtext = i.p.string
+      dict1[name]=comtext
+      
+  return dict1
+      
     
     
     
